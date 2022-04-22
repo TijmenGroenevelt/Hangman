eb601c0f16c9efe4444fb3eb1a66b13094733f5a
Tijmen Groenevelt <95089393+TijmenGroenevelt@users.noreply.github.com>
2022-04-22 13:42:35 +0000

    beide: begin galgje (woorden, letters, start, aantal letters in "_")

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ print ("Het woord heeft", ( x ) , "letters")
 streepjes = ("_ ") * (x)
 print (streepjes)
 print ("U heeft", beurten, "beurten")
+
+letters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
