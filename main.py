31a2d38c576243aa0eab8d3331c7544798a2606e
Lucas Weber <95089285+11106@users.noreply.github.com>
2022-05-13 13:41:44 +0000

    lucas: t/m regel 27 zorgen dat je 1 letter kan typen

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ print (streepjes)
 print ("U heeft", beurten, "beurten")
 
 letters = "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+
+
+s = input()
+for char in s:
+    if char.isalpha():
+      if len(s)  != 1: 
+        print("Maar 1 letter per keer :)")
+      else: 
+        print("dit is nog niet af maar de les is afgelopen")
+    else: 
+      print("nietwaar")
