7766c96f9a1cf71668ae6094ad81d09f167f8731
Lucas Weber <95089285+11106@users.noreply.github.com>
2022-06-03 13:45:52 +0000

    lucas: functies ervan gemaakt

--- main.py
+++ main.py
@@ -7,21 +7,43 @@ print("Het spel is begonnen!")
 x = (random.choice(wordlist))
 y = len(x)
 beurten = 5
-
+print(x)
 print ("Het woord heeft", ( y ) , "letters")
 
 streepjes = ("_ ") * (y)
 print (streepjes)
 print ("U heeft", beurten, "beurten")
 
-s = input()
-for char in s:
-    if char.isalpha():
-      if len(s)  != 1: 
-        print("Maar 1 letter per keer graag") 
-        break
+def gameGoed():
+  
+
+
+
+
+
+
+
+
+
+def in_voer():
+  s = input()
+  for char in s:
+      if char.isalpha():
+        if len(s)  != 1: 
+          print("Maar 1 letter per keer graag")
+          in_voer()
+          break
+        else: 
+          print("Letter", ( s ), "ingevoerd")
+          if s in x:
+            print("Die letter is goed")
+            gameGoed()
+          else:
+            print("fout")
+            gameFout()
       else: 
-        print("dit is nog niet af maar de les is afgelopen")
-    else: 
-      print("Vul een letter in")
-      break
+        print("Vul een letter in")
+        in_voer()
+        break
+in_voer()
+
